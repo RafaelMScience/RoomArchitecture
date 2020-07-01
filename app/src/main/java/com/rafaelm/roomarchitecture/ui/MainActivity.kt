a4e5b9c55fe3d7b53dfe16493a90f66cec3cb90a
RafaelM <rafaelm.science@gmail.com>
2020-07-01 19:07:49 -0400

    Feat: Insert and Retrieve Data

--- app/src/main/java/com/rafaelm/roomarchitecture/ui/MainActivity.kt
+++ app/src/main/java/com/rafaelm/roomarchitecture/ui/MainActivity.kt
@@ -2,11 +2,31 @@ package com.rafaelm.roomarchitecture.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.rafaelm.roomarchitecture.R
+import com.rafaelm.roomarchitecture.entity.Todo
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoViewModel: TodoViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        todoViewModel = ViewModelProviders.of(this).get(TodoViewModel::class.java)
+
+        todoViewModel.insertTodo(getTodoData())
+
+        todoViewModel.getTodoList()?.observe(this, Observer {
+            Log.i("MainActivity", it.toString())
+        })
+    }
+
+    private fun getTodoData(): Todo{
+        return Todo(taskName = "First Task", taskDescription = "Do it Now!")
     }
 }
